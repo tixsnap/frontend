6a1b50f28066cf3515521595309e568bad95caf3
Muhamad Gatot Supiadin <gatotbima2002@gmail.com>
2025-02-18 22:32:24 +0700

    fix: statistic

--- src/app/organizer/transaction/page.tsx
+++ src/app/organizer/transaction/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEventStore } from "@/app/store/eventStore";
 import axiosInstance from "@/app/utils/axios.helper";
+import ListNotFound from "@/components/event/ListNotFound";
+import Skeleton from "@/components/event/Skeleton";
 import Popup from "@/components/Popup";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
@@ -9,7 +11,7 @@ import { CiSearch } from "react-icons/ci";
 import { toast } from "react-toastify";
 
 export default function page() {
-  const {transactions, getTransactions} = useEventStore()
+  const {transactions, getTransactions, loading} = useEventStore()
   const [isSearch, setIsSearch] = useState<string>("")
 
   const handleSetStatus = async (txId: string, status: string) => {
@@ -39,7 +41,7 @@ export default function page() {
 
       <div className="flex gap-2 items-center border mb-5 bg-white rounded-lg w-[500] pl-5 text-sm  ">
         <CiSearch/>
-        <input className="p-2 w-full focus-within:outline-none" placeholder="Search transactions name" onChange={e => setIsSearch(e.target.value)}/>
+        <input className="p-2 w-full focus-within:outline-none" placeholder="Search transactions by customer" onChange={e => setIsSearch(e.target.value)}/>
       </div>
       <Link href={"transaction/history"} className="p-2 rounded-lg bg-gray-600 text-white text-xs hover:bg-gray-700">
         View History
@@ -48,136 +50,140 @@ export default function page() {
       <div className="w-full h-[580]">
         <Popup />
         <div className="overflow-x-auto overflow-y-auto bg-white rounded-lg shadow h-full">
-          <table className="w-full min-w-[1200px] divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Event
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Customer Name
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Status
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Created
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Expires
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Price
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Total Ticket
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Total Payment
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Payment Proof
-                </th>
-                <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {transactions && transactions.length > 0 &&
-                transactions.map((_, index) => (
-                  <tr
-                    key={index}
-                    className="hover:bg-gray-50 transition-colors duration-200"
-                  >
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {_.event.name}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {_.user.email}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      <select
-                        value={_.status}
-                        onChange={(e) => handleSetStatus(_.id, e.target.value)}
-                        className="border border-gray-300 p-2 rounded-md"
-                      >
-                        <option value="DONE">DONE</option>
-                        <option value="REJECTED">REJECTED</option>
-                        <option value="WAITING_FOR_CONFIRMATION">
-                          WAITING_FOR_CONFIRMATION
-                        </option>
-                        <option value="WAITING_PAYMENT">WAITING_PAYMENT</option>
-                        <option value="EXPIRED">EXPIRED</option>
-                        <option value="CANCELED">CANCELED</option>
-                      </select>
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {new Date(_.createdAt)
-                        .toLocaleDateString("en-GB", {
-                          day: "2-digit",
-                          month: "2-digit",
-                          year: "numeric",
-                        })
-                        .split("/")
-                        .join("-").replace(/-/g, "/")}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {new Date(_.validUntilConfirmation)
-                        .toLocaleDateString("en-GB", {
-                          day: "2-digit",
-                          month: "2-digit",
-                          year: "numeric",
-                        })
-                        .split("/")
-                        .join("-").replace(/-/g, "/")}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {`Rp ` + (_.event.price).toLocaleString('id-ID')}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {_.totalTicket}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                    {`Rp ` + (_.totalPayment).toLocaleString('id-ID')}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {_.paymentProof.paymentPicture &&
-                      _.paymentProof.paymentPicture?.length > 10 ? (
-                        <Link
-                          href={_.paymentProof.paymentPicture}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="underline text-blue-500 italic"
+
+          {
+            loading? (<Skeleton/>) : (<table className="w-full min-w-[1200px] divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Event
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Customer Name
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Status
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Created
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Expires
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Price
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Total Ticket
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Total Payment
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Payment Proof
+                  </th>
+                  <th className="px-6 py-4 text-left text-sm font-medium text-gray-500 uppercase tracking-wider">
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {transactions && transactions.length > 0 &&
+                  transactions.map((_, index) => (
+                    <tr
+                      key={index}
+                      className="hover:bg-gray-50 transition-colors duration-200"
+                    >
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {_.event.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {_.user.email}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        <select
+                          value={_.status}
+                          onChange={(e) => handleSetStatus(_.id, e.target.value)}
+                          className="border border-gray-300 p-2 rounded-md"
                         >
-                          View Proof
-                        </Link>
-                      ) : (
-                        ""
-                      )}
-                    </td>
-                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 flex gap-2">
-                      {(
-                        <div className="flex gap-2">
-                          <button
-                            onClick={() => handleSetStatus(_.id, "DONE")}
-                            className="bg-green-600 hover:bg-green-700 text-white p-2 rounded-lg"
+                          <option value="DONE">DONE</option>
+                          <option value="REJECTED">REJECTED</option>
+                          <option value="WAITING_FOR_CONFIRMATION">
+                            WAITING_FOR_CONFIRMATION
+                          </option>
+                          <option value="WAITING_PAYMENT">WAITING_PAYMENT</option>
+                          <option value="EXPIRED">EXPIRED</option>
+                          <option value="CANCELED">CANCELED</option>
+                        </select>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {new Date(_.createdAt)
+                          .toLocaleDateString("en-GB", {
+                            day: "2-digit",
+                            month: "2-digit",
+                            year: "numeric",
+                          })
+                          .split("/")
+                          .join("-").replace(/-/g, "/")}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {new Date(_.validUntilConfirmation)
+                          .toLocaleDateString("en-GB", {
+                            day: "2-digit",
+                            month: "2-digit",
+                            year: "numeric",
+                          })
+                          .split("/")
+                          .join("-").replace(/-/g, "/")}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {`Rp ` + (_.event.price).toLocaleString('id-ID')}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {_.totalTicket}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                      {`Rp ` + (_.totalPayment).toLocaleString('id-ID')}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                        {_.paymentProof.paymentPicture &&
+                        _.paymentProof.paymentPicture?.length > 10 ? (
+                          <Link
+                            href={_.paymentProof.paymentPicture}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="underline text-blue-500 italic"
                           >
-                            Accept
-                          </button>
-                          <button
-                            onClick={() => handleSetStatus(_.id, "REJECTED")}
-                            className="bg-red-600 hover:bg-red-700 text-white p-2 rounded-lg"
-                          >
-                            Reject
-                          </button>
-                        </div>
+                            View Proof
+                          </Link>
+                        ) : (
+                          ""
                         )}
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 flex gap-2">
+                        {(
+                          <div className="flex gap-2">
+                            <button
+                              onClick={() => handleSetStatus(_.id, "DONE")}
+                              className="bg-green-600 hover:bg-green-700 text-white p-2 rounded-lg"
+                            >
+                              Accept
+                            </button>
+                            <button
+                              onClick={() => handleSetStatus(_.id, "REJECTED")}
+                              className="bg-red-600 hover:bg-red-700 text-white p-2 rounded-lg"
+                            >
+                              Reject
+                            </button>
+                          </div>
+                          )}
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>)
+          }
+          
         </div>
       </div>
     </div>
