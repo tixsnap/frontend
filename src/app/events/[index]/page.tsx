59e6be74ba95d8b95173c2953e9e87a3de28db0f
Hrmn-o7 <herman_mac@HRM-MACBOOK.local>
2025-02-18 19:18:32 +0700

    event list

---
+++ src/app/events/[index]/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import { useEventStore } from "@/app/store/eventStore";
+import defaultImage from "../../../../public/event-unknown.jpg";
+import { useParams } from "next/navigation";
+
+const EventListFromSearch = () => {
+  const { getEventsByParams, events } = useEventStore();
+  const { name } = useParams();
+  // const queryParams = params?.toString();
+
+  useEffect(() => {
+    if (name) {
+      const searchParams = new URLSearchParams(name).toString();
+      // const searchParams = params.toString();
+      getEventsByParams(searchParams);
+    }
+  }, [name, getEventsByParams]);
+
+  // if (queryParams) {
+  // const params = new URLSearchParams(queryParams);
+  // const searchParams = Object.fromEntries(params);
+
+  console.log("rendering events:", events);
+
+  return (
+    <div className="sm:px-40 lg:px-20" style={{ backgroundColor: "#252A34" }}>
+      <h2 className="pt-9 text-left text-4xl font-bold  text-teal-300 mb-12 pl-5">
+        {`/${queryParams}`}
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {events.slice(0, 6).map((event) => (
+          <article
+            key={event.id}
+            className="bg-white rounded-2xl shadow-md overflow-hidden transition-transform duration-300 hover:scale-105"
+          >
+            <div className="relative h-32">
+              <Image
+                src={event.imageUrl || defaultImage}
+                alt={event.name}
+                layout="fill"
+                objectFit="cover"
+              />
+              <div className="absolute top-4 left-4 bg-black/70 text-white px-4 py-2 rounded text-sm">
+                {new Date(event.startDate).toLocaleDateString("en-US", {
+                  month: "long",
+                  day: "numeric",
+                })}
+              </div>
+            </div>
+            <div className="p-6">
+              <h3 className="text-xl font-semibold text-gray-800 mb-4">
+                {event.name}
+              </h3>
+              <p className="text-gray-600 text-base leading-relaxed">
+                {event.description}
+              </p>
+            </div>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+  // } else {
+  // If there are no query parameters, redirect to a different page or render a different component
+  // return <div>{`nothing found ${queryParams}`}</div>;
+  // }
+};
+
+export default EventListFromSearch;
